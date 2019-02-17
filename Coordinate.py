292d0f43d5076fd63e42814b12b4e004b21c2f21
GrannyG <genrikh.askaryan@mail.utoronto.ca>
2019-02-16 23:45:23 -0500

    Ship factory and coordinate class created

---
+++ Coordinate.py
@@ -0,0 +1,15 @@
+
+
+#class to represent a square on the battleshipboard
+class Coordinate:
+    #Initialize the class with a letter and a number
+    def __init__(self, lettercord, numbercord):
+        self.lettercord = lettercord
+        self.numbercord = numbercord
+        
+    #Return the letter
+    def get_letter():
+        return self.lettercord
+    #Return the number
+    def get_number():
+        return self.numbercord
