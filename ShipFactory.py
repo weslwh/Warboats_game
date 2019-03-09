f5500d4d5804fd437d1551c759f915066d334f92
ATBM <arnaud.michel@mail.utoronto.ca>
2019-03-09 08:50:45 -0800

    Add docstrings to ShipFactory.py

--- ShipFactory.py
+++ ShipFactory.py
@@ -2,12 +2,30 @@ import Ship
 
 
 class ShipFactory:
-	#The ship factory returns a built ship given the
-	#Information the user specifies
-	def build(typ, coordinate):
-		s = Ship()
-		s.set_ship_coordinates(coordinate)
-		s.set_type(typ)
-		return s
-	
-	
+    """
+    The ShipFactory class builds Ship objects
+    
+    Methods:
+        build(ship_type, coordinates)
+    """
+    def build(ship_type, coordinates):
+        """
+        Builds and returns a Ship object with the specified type and 
+        coordinates.
+        
+        Args:
+            type (String): The type of the ship to be built.
+            coordinates (List[Coordinate]): The coordinates of the ship to be built.
+            
+        Returns:
+            build_ship (Ship): The ship that was built.
+        
+        Raises:
+            void
+        """
+        s = Ship()
+        s.set_ship_coordinates(coordinates)
+        s.set_type(ship_type)
+        return s
+
+
