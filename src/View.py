b62348852efc8d6c6102e4ef73d67b4666ee022d
askarya2 <genrikh.askaryan@mail.utoronto.ca>
2019-03-17 21:58:20 -0400

    Update the graphics in view

--- src/View.py
+++ src/View.py
@@ -1,66 +1,89 @@
+"""
+ Example program to show using an array to back a grid on-screen.
+ 
+ Sample Python/Pygame Programs
+ Simpson College Computer Science
+ http://programarcadegames.com/
+ http://simpson.edu/computer-science/
+ 
+ Explanation video: http://youtu.be/mdTeqiWyFnc
+"""
 import pygame
-
-pygame.init()
-
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
+ 
+# Define some colors
 BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+ 
+# This sets the WIDTH and HEIGHT of each grid location
+WIDTH = 20
+HEIGHT = 20
 
-size = (700, 700)
-
-#initizalize the screen to the given size
-screen = pygame.display.set_mode(size)
+# This sets the margin between each cell
+MARGIN = 5
+ 
+# Create a 2 dimensional array. A two dimensional
+# array is simply a list of lists.
+grid = []
+grid2 = []
+for row in range(10):
+    # Add an empty array that will hold each cell
+    # in this row
+    grid.append([])
+    grid2.append([])
+    for column in range(10):
+        grid[row].append(0)  # Append a cell
+        grid2[row].append(0)
+ 
+# Initialize pygame
+pygame.init()
+ 
+# Set the HEIGHT and WIDTH of the screen
+WINDOW_SIZE = [720, 600]
+screen = pygame.display.set_mode(WINDOW_SIZE)
 
 pygame.display.set_caption("Warboats")
 
 done = False
-
+ 
+# Used to manage how fast the screen updates
 clock = pygame.time.Clock()
-
-
-#game loop
-HEIGHT = 20
-WIDTH = 20
-MARGIN = 5
-grid = [[0 for x in range(10)] for y in range(10)]
+ 
 
 while not done:
-
-    #pygame.display.update()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            done = True  # Flag that we are done so we exit this loop
         elif event.type == pygame.MOUSEBUTTONDOWN:
             # User clicks the mouse. Get the position
             pos = pygame.mouse.get_pos()
             # Change the x/y screen coordinates to grid coordinates
-            column = (pos[0] // (WIDTH + MARGIN)) + 1
-            row = (pos[1] // (HEIGHT + MARGIN)) + 1
+            column = pos[0] // ((WIDTH + MARGIN))
+            row = pos[1] // ((HEIGHT + MARGIN))
             # Set that location to one
-            grid[row][column] = 1
+            grid2[row][column] = 1
             print("Click ", pos, "Grid coordinates: ", row, column)
-            
-    #Game logic should go here
-        
-        
+ 
+    # Set the screen background
     screen.fill(BLACK)
-    #pygame.display.flip()
+ 
+    # Draw the grid
     for row in range(10):
         for column in range(10):
             color = WHITE
             if grid[row][column] == 1:
                 color = GREEN
-            pygame.draw.rect(screen,
-                                 color,
-                                 [(MARGIN + WIDTH) * (column - 1) + MARGIN,
-                                  (MARGIN + HEIGHT) * (row - 1) + MARGIN,
-                                  WIDTH,
-                                  HEIGHT])        
-        
-        clock.tick(60)
-        pygame.display.flip()
-        
-        
-pygame.quit()       
-
+            pygame.draw.rect(screen,color, [(MARGIN + WIDTH) * column + MARGIN, (MARGIN + HEIGHT) * row + MARGIN,WIDTH, HEIGHT])
+            pygame.draw.rect(screen, color, [(MARGIN + WIDTH) * column + MARGIN, (MARGIN + HEIGHT) * row + MARGIN+ 300,WIDTH, HEIGHT])
+            pygame.draw.rect(screen, RED, (0,252, 250, 50), 0)
+            
+    # Limit to 60 frames per second
+    clock.tick(60)
+ 
+    # Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+# Be IDLE friendly. If you forget this line, the program will 'hang'
+# on exit.
+pygame.quit()
