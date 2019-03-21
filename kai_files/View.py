8d8e05bd614b53e61d228a328d90aeef41103ea7
haujame1 <haujame1@mai.utoronto.ca>
2019-03-21 12:40:02 -0400

    Add start screen to new View
    Added the start screen to our new version of View. This allows the
    users to decide if they want to play the game, and presents them with
    the game's title. We can also add instruction functionality to the
    start screen in a future commit

--- kai_files/View.py
+++ kai_files/View.py
@@ -1,10 +1,49 @@
 import Constants
 import Grid
 import Game_Engine
+import time
 
 import pygame
 pygame.init() # initialize the pygame module
 
+
+#get the textready
+screen = pygame.display.set_mode(Constants.SCREEN_SIZE)
+clock = pygame.time.Clock()
+font = pygame.font.SysFont('Comic Sans MS', 30)
+text = font.render('Welcome to Warboats!', False,Constants.BLUE)
+font2 = pygame.font.SysFont('Comic Sans MS', 15)
+text2 = font2.render('Press C to continue, or Q to quit', False, Constants.BLUE)
+
+start = False
+while not start:
+	screen.fill(Constants.WHITE)
+	screen.blit(text, (350,200))
+	screen.blit(text2, (380, 250))
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			start = True
+			pygame.quit()
+			quit()
+		if event.type == pygame.KEYDOWN:		
+			if event.key == pygame.K_q:
+				start = True
+				pygame.quit()
+				quit()
+			if event.key == pygame.K_c:
+				start = True
+				
+
+
+
+	pygame.display.update()
+
+	clock.tick(30)
+	pygame.display.flip()
+
+
+
+pygame.display.update()
 
 
 
